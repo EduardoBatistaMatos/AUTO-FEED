e6d64b8d7ff488e2ad7ae61029a5228038939c98
Eduardo Batista <dudubmatos@gmail.com>
2024-11-08 21:59:33 -0300

    Commit *22

--- BackEnd/api/models/criarcontaModels.js
+++ BackEnd/api/models/criarcontaModels.js
@@ -7,7 +7,7 @@ module.exports = {
 }
 
 function criarConta(nome, idade, usuario, senha, callback) {
-    const m_sql = "INSERT INTO contas (nome, idade, usuario, senha) VALUES (?, ?, ?, ?)";
+    const m_sql = "INSERT INTO individuo (nome, idade, usuario, senha) VALUES (?, ?, ?, ?)";
     conexao.query(m_sql, [nome, idade, usuario, senha], callback);
 }
 
